6ef2a7ae57771aad6214d32f514049f96d33014f
zjx <panda@pandadeMacBook-Pro.local>
2016-09-04 21:17:22 +0800

    去除与测试工具无关代码，haproxy已生成新repo

--- main.go
+++ main.go
@@ -3,21 +3,9 @@ package main
 import (
 	"fmt"
 	WorkLoadController "./workload"
-	"net/http"
 )
 
-func HelloServer(w http.ResponseWriter, req *http.Request) {
-	if req.Method == "GET" {
-		http.Redirect(w, req, "http://202.120.40.175:21101/users/all", http.StatusTemporaryRedirect)
-	} else {
-		http.Redirect(w, req, "http://202.120.40.175:21101/users", http.StatusTemporaryRedirect)
-	}
-}
-
 func main() {
-	http.HandleFunc("/hello", HelloServer)
-	http.ListenAndServe(":12345", nil)
-
 
 	//Request.GenetatePodBody("default", "mysql:5.7", "mysql----test")
 	//Request.CreatePod_test("default", "mysql:5.7", "mysql----test", "300m", "400m", "1Gi", "2Gi")

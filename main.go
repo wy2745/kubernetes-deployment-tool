3db25565d5e250d1fadb7a71a2e71e1af291b9df
zjx <panda@pandadeMacBook-Pro.local>
2016-09-04 20:53:48 +0800

    稍微试了一下转发请求，请求转发方案可行，可以用于写go版本的myHaproxy

--- main.go
+++ main.go
@@ -3,9 +3,21 @@ package main
 import (
 	"fmt"
 	WorkLoadController "./workload"
+	"net/http"
 )
 
+func HelloServer(w http.ResponseWriter, req *http.Request) {
+	if req.Method == "GET" {
+		http.Redirect(w, req, "http://202.120.40.175:21101/users/all", http.StatusTemporaryRedirect)
+	} else {
+		http.Redirect(w, req, "http://202.120.40.175:21101/users", http.StatusTemporaryRedirect)
+	}
+}
+
 func main() {
+	http.HandleFunc("/hello", HelloServer)
+	http.ListenAndServe(":12345", nil)
+
 
 	//Request.GenetatePodBody("default", "mysql:5.7", "mysql----test")
 	//Request.CreatePod_test("default", "mysql:5.7", "mysql----test", "300m", "400m", "1Gi", "2Gi")
@@ -21,5 +33,7 @@ func main() {
 	//Request.GetAllReplicationcontrollers_Test()
 	//Request.CreateReplicationController_test("default", "mysql:5.7", "mysql-rpc", "mysql-test", "mysql----test", 2)
 	//Request.DeleteReplicationController("default", "mysql-rpc")
+
+
 }
 

b569abe2685ff1ca4427439146d77b14711dbfbc
zjx <271540124@qq.com>
2016-10-10 17:01:40 +0800

    no message

--- changeNode.sh
+++ changeNode.sh
@@ -3,6 +3,7 @@
 nodenum=$1
 
 IP=192.168.6.10
+echo "haha"
 
 ssh ${IP} "cd /home/administrator/resources/ && kubectl delete rc hollow-node --namespace=kubemark && kubectl create -f hollow-node"+"${nodenum}.json --namespace=kubemark"
 

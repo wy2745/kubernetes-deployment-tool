d72d7d7f7c522b0327de7664a82381b14befa369
zjx <271540124@qq.com>
2016-10-10 17:09:13 +0800

    寻找kubectl

--- changeNode.sh
+++ changeNode.sh
@@ -3,7 +3,8 @@
 nodenum=$1
 
 IP=192.168.6.10
+kubectl=/home/administrator/kubernetes/cluster/ubuntu/binaries/kubectl
 echo "haha"
 
-ssh ${IP} "cd /home/administrator/resources/ && kubectl delete rc hollow-node --namespace=kubemark && kubectl create -f hollow-node"+"${nodenum}.json --namespace=kubemark"
+ssh ${IP} "cd /home/administrator/resources/ && ${kubectl} delete rc hollow-node --namespace=kubemark && ${kubectl} create -f hollow-node"+"${nodenum}.json --namespace=kubemark"
 
--- kubemark/tester.go
+++ kubemark/tester.go
@@ -233,7 +233,7 @@ func PodListTest() {
 
 func ShTest() {
 	DeleteNodev2()
-	cmd := exec.Command("/bin/sh", "-c", "~/go/src/github.com/wy2745/kubernetes-deployment-tool/changeNode.sh 5")
+	cmd := exec.Command("/bin/sh", "-c", "~/go/src/github.com/wy2745/kubernetes-deployment-tool/changeNode.sh 10")
 	fmt.Println(cmd.Output())
 	fmt.Println("好了!!!!!!!")
 }

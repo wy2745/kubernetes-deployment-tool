9f184b7df9c0b356fc4ac5ce54e5de58f1102837
zjx <271540124@qq.com>
2016-10-12 23:10:20 +0800

    切换node数量的时候在kubernetes集群删除对应的hollow pod

--- changeNode.sh
+++ changeNode.sh
@@ -9,5 +9,9 @@ kubectl=/home/administrator/kubernetes/cluster/ubuntu/binaries/kubectl
 echo "haha"
 
 ssh ${IP} "cd /home/administrator/resources/ && ${kubectl} delete rc hollow-node --namespace=kubemark"
+
+while [[ ! -z "$(${kubectl} get pods --show-all --namespace=kubemark| tail -n +1)" ]]; do
+    sleep 5
+  done
 ssh ${IP} "${kubectl} create -f /home/administrator/resources/hollow-node${nodenum}.json --namespace=kubemark"
 

b9987ff7b50b2cf165e4ace72251136b7d4d84fc
zjx <271540124@qq.com>
2016-10-13 14:28:33 +0800

    修改一哈测试参数

--- locustTest.sh
+++ locustTest.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 
-for hatch_rate in 10 100 1000 10000 100000 1000000
+for hatch_rate in 10 100 1000 10000
 do
-    for locust_count in 10 100 1000 10000 100000 1000000
+    for locust_count in 10 100 1000 10000
     do
         ./kubernetes-deployment-tool -l 3 ${locust_count} ${hatch_rate}
         done

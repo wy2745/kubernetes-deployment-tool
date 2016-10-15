fc5d014aaba60e1f85d4f3eaca6cfdd0bbd203c6
zjx <271540124@qq.com>
2016-10-15 12:29:28 +0800

    no message

--- loadBalanceTest.sh
+++ loadBalanceTest.sh
@@ -27,11 +27,11 @@ do
 
     sleep 10
     echo "进行server的ab测试"
-    ssh ${teserIP} "cd ${fileroot} && ab -k -n 100000 -c 50 -e ${servicefileName}.csv -g ${servicefileName}.gnp ${serviceUrl} > ${servicefileName}.html"
+    ssh ${teserIP} "cd ${fileroot} && ab -k -n 100000 -c 20 -e ${servicefileName}.csv -g ${servicefileName}.gnp ${serviceUrl} > ${servicefileName}.html"
     sleep 60
     echo "测试完成"
     echo "进行apiserver的ab测试"
-    ssh ${teserIP} "cd ${fileroot} && ab -k -n 100000 -c 50 -e ${apifileName}.csv -g ${apifileName}.gnp ${apiUrl} > ${apifileName}.html"
+    ssh ${teserIP} "cd ${fileroot} && ab -k -n 100000 -c 20 -e ${apifileName}.csv -g ${apifileName}.gnp ${apiUrl} > ${apifileName}.html"
     sleep 60
     echo "测试完成"
     arr=($(kubectl get pods | grep nginx | awk '{print $1};' | tail -n +1))
@@ -45,7 +45,7 @@ do
     echo "配置完成"
     sleep 10
     echo "进行外部loadbalancer的ab测试"
-    ssh ${teserIP} "cd ${fileroot} && ab -k -n 100000 -c 50 -e ${eloadfileName}.csv -g ${eloadfileName}.gnp ${eloadbUrl} > ${eloadfileName}.html"
+    ssh ${teserIP} "cd ${fileroot} && ab -k -n 100000 -c 20 -e ${eloadfileName}.csv -g ${eloadfileName}.gnp ${eloadbUrl} > ${eloadfileName}.html"
     sleep 60
     echo "测试完成"
     done

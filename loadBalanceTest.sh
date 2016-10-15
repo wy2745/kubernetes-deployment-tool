9e30128b1db23e505a1b621dbc0812774f293a3a
zjx <271540124@qq.com>
2016-10-15 10:10:37 +0800

    修改nginx机器ip

--- loadBalanceTest.sh
+++ loadBalanceTest.sh
@@ -3,10 +3,11 @@
 echo "开始测试"
 
 teserIP=192.168.6.15
-nginxIp=192.168.6.31
+nginxIp=192.168.6.14
 serviceUrl=http://192.168.6.22:30080/index.html
 apiUrl=http://192.168.6.10:8080/api/v1/proxy/namespaces/default/services/nginx-svc:80/index.html
 eloadbUrl=http://192.168.6.31/index.html
+fileroot=lbtest
 
 kubectl=/home/administrator/kubernetes/cluster/ubuntu/binaries/kubectl
 
@@ -26,11 +27,11 @@ do
 
     sleep 10
     echo "进行server的ab测试"
-    ssh ${teserIP} "cd test && ab -k -n 100000 -c 50 -e ${servicefileName}.csv -g ${servicefileName}.gnp ${serviceUrl} > ${servicefileName}.html"
+    ssh ${teserIP} "cd ${fileroot} && ab -k -n 100000 -c 50 -e ${servicefileName}.csv -g ${servicefileName}.gnp ${serviceUrl} > ${servicefileName}.html"
     sleep 60
     echo "测试完成"
     echo "进行apiserver的ab测试"
-    ssh ${teserIP} "cd test && ab -k -n 100000 -c 50 -e ${apifileName}.csv -g ${apifileName}.gnp ${apiUrl} > ${apifileName}.html"
+    ssh ${teserIP} "cd ${fileroot} && ab -k -n 100000 -c 50 -e ${apifileName}.csv -g ${apifileName}.gnp ${apiUrl} > ${apifileName}.html"
     sleep 60
     echo "测试完成"
     arr=($(kubectl get pods | grep nginx | awk '{print $1};' | tail -n +1))
@@ -44,7 +45,7 @@ do
     echo "配置完成"
     sleep 10
     echo "进行外部loadbalancer的ab测试"
-    ssh ${teserIP} "cd test && ab -k -n 100000 -c 50 -e ${eloadfileName}.csv -g ${eloadfileName}.gnp ${eloadbUrl} > ${eloadfileName}.html"
+    ssh ${teserIP} "cd ${fileroot} && ab -k -n 100000 -c 50 -e ${eloadfileName}.csv -g ${eloadfileName}.gnp ${eloadbUrl} > ${eloadfileName}.html"
     sleep 60
     echo "测试完成"
     done
